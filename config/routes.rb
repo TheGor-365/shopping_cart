1b3ffb3d0d0596efd1f6e577a621a4e0fe93868d
Gor <7495500@gmail.com>
2023-01-29 00:12:11 +0300

    Create cart with turbo rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'products#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get  'cart', to: 'cart#show'
+  post 'cart/add'
+  post 'cart/remove'
+
+  resources :products
 end
